eb12aa37df0507d57c9febcd708ec4b9a5cf1491
mlheur <mlheur@gmail.com>
2026-01-05 13:13:27 -0800

    feat: unit test automation script

---
+++ unit_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+function unit_test() {
+    F=$1; shift
+    echo "################################################################################" >&2
+    echo "# Testing [${F}]" >&2
+    echo "################################################################################" >&2
+    python3 "${F}"; RC=$?
+    echo "################################################################################" >&2
+    echo "# RC=[${RC}]" >&2
+    echo "################################################################################" >&2
+    echo  >&2
+    return ${RC}
+}
+
+FinalRC=0
+for F in *.py; do
+    SKIP=0
+    for S in "Kanoodle" "Solver"; do
+        if [ "${F}" = "${S}.py" ]; then SKIP=1; break; fi
+    done
+    if [ ${SKIP} -eq 0 ]; then
+        unit_test $F; RC=$?
+        if [ ${RC} -ne 0 ]; then FinalRC=1; fi
+    fi
+done
+if [ ${FinalRC} -ne 0 ]; then
+    echo "FATAL: Some tests failed" >&2
+    exit 1
+fi
+#exit 0
